6faf81bdcb819b54e1f9223469d7dca37f6f8075
YogeshSeemakurthi <ext-yseemakurthi@exo.inc>
2025-06-29 19:31:40 +0530

    initial commit

--- manipulate.sh
+++ manipulate.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-if [ ${{inputs.deployment_env}} == "dev" ];then
+if [ "${{inputs.deployment_env}}" == "dev" ];then
     sed -i '/<head>/a\
     dev' index.html
-elif [ ${{inputs.deployment_env}} == "qa" ];then
+elif [ "${{inputs.deployment_env}}" == "qa" ];then
     sed -i '/<head>/a\
     qa' index.html
 fi
